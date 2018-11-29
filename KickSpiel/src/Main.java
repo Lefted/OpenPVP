b249d0bc935aa7b97d218bcf1a66491a28804626
Steffen <Steffen@DESKTOP-EAMSFBJ.fritz.box>
2018-11-29 20:45:00 +0100

    Hallo
    Change-Id: I92d9db837673bce0be5291a9271918915069ee6e
    Signed-off-by: Gartn <Gartn@PCMoritz.fritz.box>

--- KickSpiel/src/Main.java
+++ KickSpiel/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hallo Steffan!");
 		System.out.println("Mit beiden Soﬂen?");
+		System.out.println("Tschuss Junge Mann");
+		
+		System.out.println("Ariciderci");
 	}
 }
